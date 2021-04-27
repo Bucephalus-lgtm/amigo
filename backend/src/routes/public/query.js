df01c06e777d106e1b918ad3b99832aa63650440
Bucephalus <bhargabnath691@gmail.com>
2021-04-27 16:24:21 +0530

    get queries route added

--- backend/src/routes/public/query.js
+++ backend/src/routes/public/query.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { send_queries } = require("../../controllers/query");
+const { send_queries, receive_queries, } = require("../../controllers/query");
 const router = express.Router();
 
 router.post("/send-queries", send_queries);
+router.get("/receive-queries", receive_queries);
 
 module.exports = router;
